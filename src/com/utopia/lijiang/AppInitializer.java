fa8e6f4213efdef32729dac67384aa1e26f7cd5b
chao-zhou <eebread@gmail.com>
2012-01-09 11:52:20 +0800

    add comments

--- src/com/utopia/lijiang/AppInitializer.java
+++ src/com/utopia/lijiang/AppInitializer.java
@@ -10,43 +10,58 @@ import com.utopia.lijiang.global.Status;
 import com.utopia.lijiang.location.LocationUtil;
 import com.utopia.lijiang.service.LocationService;
 import com.utopia.lijiang.util.NotificationUtil;
-
+/** Initial global variables in this application. 
+ * Like starting services, reading data from SQLite, configuring settings
+ * @author chao_zhou
+ * @version 1.0.0.0
+ * */
 public class AppInitializer {
 
 	private Context ctx = null;
 	
+	/**Run all initial works
+	 * @param First loaded context
+	 * */
 	public static void doWork(Context context){
 		AppInitializer instance = new AppInitializer(context);
 		instance.doWork();
 	}
 	
+	/**Constructor
+	 * @param First loaded context
+	 * */
 	public AppInitializer(Context context){
 		ctx = context;
 	}
 	
+	/**Run all initial works*/
  	private void doWork(){
 		configNotification();
-		configLastKnownLocation();
+		getLastKnownLocation();
 		loadAlarm();
 		startLocationService();
 	}
 	
+ 	/**Reload Alarm Information from SQLite */
  	private void loadAlarm(){
  		AlarmManager.getInstance().reset();
  		AlarmManager.getInstance().load4DB(ctx, SimpleAlarm.class);
  	}
  	
+ 	/**Configure Notification's setting*/
 	private void configNotification(){
 	    	NotificationUtil.setIcon(R.drawable.ic_launcher);
 	    	NotificationUtil.setTickerText(ctx.getString(R.string.app_name));
 	}
-	  
-	private Location configLastKnownLocation(){
+	
+	/**Get last known location of device*/
+	private Location getLastKnownLocation(){
 	    	Location loc = LocationUtil.getBestLastKnowLocation(ctx);	
 	    	Status.getCurrentStatus().setLocation(loc);	
 	    	return loc;
 	}
-	    
+	
+	/**Start Location Service*/
 	private void startLocationService(){
 	    	Intent intent = new Intent(ctx,LocationService.class);
 	    	ctx.startService(intent);  
--- src/com/utopia/lijiang/LijiangActivity.java
+++ src/com/utopia/lijiang/LijiangActivity.java
@@ -3,9 +3,7 @@ package com.utopia.lijiang;
 import com.utopia.lijiang.alarm.Alarm;
 import com.utopia.lijiang.alarm.AlarmListener;
 import com.utopia.lijiang.alarm.AlarmManager;
-import com.utopia.lijiang.alarm.LocationAlarm;
 import com.utopia.lijiang.global.Status;
-import com.utopia.lijiang.location.LocationUtil;
 
 import android.app.ListActivity;
 import android.location.Location;
@@ -16,7 +14,18 @@ import android.widget.Button;
 
 import android.widget.TextView;
 import android.content.Intent;
-
+/** Main Activity of the application.
+ * When this activity is created, the application's global variables will be initialed.
+ * And a location service will be started. 
+ * But this service will be not closed, when this activity is finished.
+ * 
+ * This activity has a singleton method. 
+ * So you can get last instance of this activity anywhere in the application.
+ * For example: <i>LijiangActivity.getLatestInstance().setTaskInfoText();</i>
+ * 
+ * @author chao_zhou
+ * @version 1.0.0.0
+ * */
 public class LijiangActivity extends ListActivity  {
 	
 	private static LijiangActivity instance = null;
@@ -26,7 +35,10 @@ public class LijiangActivity extends ListActivity  {
 	
 	private AlarmListener alarmListener = null;
 	
-	
+	/**
+	 * Singleton method, this will be update after this activity is launched
+	 * @return The Latest showed LijiangActivity
+	 */
 	public static LijiangActivity getLatestInstance(){
 		return instance;
 	}
@@ -40,11 +52,12 @@ public class LijiangActivity extends ListActivity  {
     	setContentView(R.layout.main);
 	
     	AppInitializer.doWork(this);    	
-    	initialVariables();
-    	bindToAlarmManager();
+    	this.initialVariables();
+    	bindToAlarmManager();//Just for developing, should be removed in release version
     	
     }    
     
+    /**Called when the activity is active*/
     @Override
     public void onStart(){
     	Log.d(getString(R.string.debug_tag),"Start LijiangActivity ");
@@ -53,6 +66,7 @@ public class LijiangActivity extends ListActivity  {
     	bindList();
     }
     
+    /**Called when the activity is finished*/
     @Override
     public void onDestroy(){
     	Log.d(getString(R.string.debug_tag),"Destroy LijiangActivity");
@@ -60,28 +74,30 @@ public class LijiangActivity extends ListActivity  {
     	super.onDestroy();
     }     
     
-    public void setText(String text){
+    /** Set All Running Task's Information.
+     * @param Task's Information
+     * */
+    public void setTaskInfoText(String text){
     	 	taskInfo.setText(text);
     }  
     
+    /** Do work after showAddAlarm button is clicked
+     * @param trigger
+     * */
     public void showAddAlarmActivity(View target){
     	this.startActivity(new Intent(this,AddSimpleAlarmActivity.class));
     }
     
-    //Have no relative with UI
+    /**Fill Data into Task List*/
+	private void bindList(){ 
+	       AlarmAdapter adapter = 
+	    		   new AlarmAdapter(this,AlarmManager.getInstance().getAllAlarm());
+	       setListAdapter(adapter);
+	}
     
-    private void bindToAlarmManager(){
-	    alarmListener = new AlarmListener(){
-				@Override
-		public void onAlarm(Alarm alarm) {
-					String msg = alarm.getMessage();
-					setText(msg);
-				}
-	    };
-	    	
-	   AlarmManager.getInstance().addAlarmListener(alarmListener);	
-	} 
-	 
+    //Have no relative with UI
+	
+	/**Initial Variables used in this activity*/
     private void initialVariables(){
     	instance = this;
     	taskInfo = (TextView)findViewById(R.id.taskInfo);
@@ -91,23 +107,43 @@ public class LijiangActivity extends ListActivity  {
     	//Location testLoaction = LocationUtil.createLijingLocation(38, -112, 0, 0);	
     	//AlarmManager.getInstance().addAlarm(new LocationAlarm("Test Alarm",testLoaction));
     }
+	
+	
+    /**Bind listener to all alarms. 
+     * Just for developing, should be removed in release version
+     * */
+    private void bindToAlarmManager(){
+	    alarmListener = new AlarmListener(){
+				@Override
+		public void onAlarm(Alarm alarm) {
+					String msg = alarm.getMessage();
+					setTaskInfoText(msg);
+				}
+	    };
+	    	
+	   AlarmManager.getInstance().addAlarmListener(alarmListener);	
+	} 
+	
+  
     
+    /** Unbind listener to all alarms 
+     * Just for developing, should be removed in release version
+     * */
 	private void unbindFromAlarmManager(){
 	    	AlarmManager.getInstance().removeAlarm(alarmListener);
 	    	alarmListener = null;
 	}
 	
+	/** Show location information in task info view
+	 * Just for developing, should be removed in release version
+	 * */
 	private void showLocation(){
     	Location loc = Status.getCurrentStatus().getLocation();	
     	if(loc!=null){
-    		setText(loc.toString());
+    		setTaskInfoText(loc.toString());
     	}
 	}
 	
-	private void bindList(){ 
-       AlarmAdapter adapter = 
-    		   new AlarmAdapter(this,AlarmManager.getInstance().getAllAlarm());
-       setListAdapter(adapter);
-	}
+
 	
 }
