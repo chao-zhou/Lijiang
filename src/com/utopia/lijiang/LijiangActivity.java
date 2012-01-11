fd0e3ebeb2245aecaa07abde1ff54d19601602ef
chao-zhou <eebread@gmail.com>
2012-01-11 16:37:40 +0800

    remove unused import

--- src/com/utopia/lijiang/LijiangActivity.java
+++ src/com/utopia/lijiang/LijiangActivity.java
@@ -10,7 +10,6 @@ import android.location.Location;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 
 import android.widget.TextView;
 import android.content.Intent;
