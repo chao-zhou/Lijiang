7c18e6b1714427515f1f485275c77d9451791970
chao-zhou <eebread@gmail.com>
2012-02-09 17:07:49 +0800

    New Auto Created R.java

--- gen/com/utopia/lijiang/R.java
+++ gen/com/utopia/lijiang/R.java
@@ -15,18 +15,16 @@ public final class R {
     }
     public static final class drawable {
         public static final int alarm_item=0x7f020000;
-        public static final int blue_color=0x7f02000a;
-        public static final int default_color=0x7f020008;
-        public static final int green_color=0x7f02000b;
+        public static final int blue_color=0x7f020008;
+        public static final int default_color=0x7f020006;
+        public static final int green_color=0x7f020009;
         public static final int ic_launcher=0x7f020001;
-        public static final int icon=0x7f020002;
-        public static final int iconmarka=0x7f020003;
-        public static final int marker_rounded_blue=0x7f020004;
-        public static final int marker_rounded_grey=0x7f020005;
-        public static final int marker_rounded_red=0x7f020006;
-        public static final int pop=0x7f020007;
-        public static final int pressed_color=0x7f02000c;
-        public static final int red_color=0x7f020009;
+        public static final int marker_rounded_blue=0x7f020002;
+        public static final int marker_rounded_grey=0x7f020003;
+        public static final int marker_rounded_red=0x7f020004;
+        public static final int pop=0x7f020005;
+        public static final int pressed_color=0x7f02000a;
+        public static final int red_color=0x7f020007;
     }
     public static final class id {
         public static final int addAlarmActive=0x7f070002;
@@ -79,7 +77,8 @@ public final class R {
         public static final int guide=0x7f04000d;
         public static final int hello=0x7f04000e;
         public static final int mapEditTextHint=0x7f04000f;
-        public static final int renew=0x7f040010;
-        public static final int searching=0x7f040011;
+        public static final int myLocation=0x7f040010;
+        public static final int renew=0x7f040011;
+        public static final int searching=0x7f040012;
     }
 }
