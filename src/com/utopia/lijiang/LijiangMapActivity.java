5c08c5a9b5f6b1b5f567ea1b24e18b4fddeb0211
chao-zhou <eebread@gmail.com>
2012-03-26 17:36:18 +0800

    Move overlay logic to LijiangOverlayMapActivity
    This class only do search and add alarms

--- src/com/utopia/lijiang/LijiangMapActivity.java
+++ src/com/utopia/lijiang/LijiangMapActivity.java
@@ -1,28 +1,18 @@
 package com.utopia.lijiang;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Observable;
-import java.util.Observer;
-
 import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
-import android.location.Location;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
-import android.view.ViewGroup.LayoutParams;
 import android.view.inputmethod.EditorInfo;
 import android.view.inputmethod.InputMethodManager;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.TextView.OnEditorActionListener;
 import android.widget.Toast;
 
-import com.baidu.mapapi.GeoPoint;
 import com.baidu.mapapi.MKAddrInfo;
 import com.baidu.mapapi.MKDrivingRouteResult;
 import com.baidu.mapapi.MKPoiResult;
@@ -30,35 +20,15 @@ import com.baidu.mapapi.MKSearch;
 import com.baidu.mapapi.MKSearchListener;
 import com.baidu.mapapi.MKTransitRouteResult;
 import com.baidu.mapapi.MKWalkingRouteResult;
-import com.baidu.mapapi.MapView;
-import com.baidu.mapapi.OverlayItem;
-import com.utopia.lijiang.baidu.BaiduItemizedOverlay;
-import com.utopia.lijiang.baidu.BaiduLongPressItemizedOverlay;
-import com.utopia.lijiang.baidu.BaiduMapActivity;
 import com.utopia.lijiang.global.Status;
 import com.utopia.lijiang.view.SafeProgressDialog;
 
-public class LijiangMapActivity extends BaiduMapActivity implements Observer{
-
-	static String CURRENT_CITY = "";
-	static MapView mMapView = null;
-	static View mPopView = null;	
-	final static int MAX_SEARCHING_SECOND = 1000*5;
-	
+public class LijiangMapActivity extends LijiangOverlayActivity {
 	MKSearch mSearch = null;
 	EditText poiNameEditText = null;
 	InputMethodManager imm = null;
 	ProgressDialog progressDialog = null;
-	TextView popName = null;
-	TextView popAddress = null;
-	Button popSave = null;
-	
-	BaiduItemizedOverlay userOverlay = null;
-	BaiduItemizedOverlay searchOverlay = null;
-	BaiduItemizedOverlay customOverlay = null;
-	
-	GeoPoint latestPoint = null;
-	
+		
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
@@ -69,8 +39,10 @@ public class LijiangMapActivity extends BaiduMapActivity implements Observer{
 	    initialSearchInput();
 	    initialSearch();
 	    
-	    //Listen Status' change
+	    locateCurrentCenter();
+		//Listen Status' change
 	    Status.getCurrentStatus().addObserver(this);
+
 	}
 
 	@Override
@@ -96,52 +68,6 @@ public class LijiangMapActivity extends BaiduMapActivity implements Observer{
 		return R.layout.baidumap;
 	}
 	
-	@Override
-	public MapView getMapView() {
-		// TODO Auto-generated method stub
-		return mMapView;
-	}
-
-	@Override
-	public boolean onTapped(int i, OverlayItem item) {
-		// TODO Auto-generated method stub
-		Log.d("lijiang","onTapped");
-		popName.setText(item.getTitle());
-		popAddress.setText(item.getSnippet());
-		
-		latestPoint = item.getPoint();
-		
-		mMapView.getController().setCenter(item.getPoint());	
-		LijiangMapActivity.mMapView.updateViewLayout( LijiangMapActivity.mPopView,
-                new MapView.LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT,
-                		item.getPoint(), 0,-30,MapView.LayoutParams.BOTTOM_CENTER));
-		LijiangMapActivity.mPopView.setVisibility(View.VISIBLE);
-		return true;
-	}
-
-	@Override
-	public void onTapping(GeoPoint pt, MapView v) {
-		// TODO Auto-generated method stub
-		Log.d("lijiang","onTapping");
-		LijiangMapActivity.mPopView.setVisibility(View.GONE);
-	}
-
-	@Override
-	public void update(Observable observable, Object data) {
-		// TODO Auto-generated method stub
-		Location loc = Status.getCurrentStatus().getLocation();
-		GeoPoint pt = new GeoPoint((int)loc.getLatitude(), (int)loc.getLongitude());
-		String title = this.getString(R.string.myLocation);
-		String message = pt.getLatitudeE6()+":"+pt.getLongitudeE6();
-		OverlayItem item = new OverlayItem(pt,title,message);
-		
-		List<OverlayItem> items = new ArrayList<OverlayItem>();
-		items.add(item);
-		userOverlay.setItems(items);
-		
-		//mMapView.getController().setCenter(item.getPoint());
-	}
-
 	public void addAlarm(View target){
 		 String title = popName.getText().toString();
 		 String message = popAddress.getText().toString();
@@ -149,12 +75,12 @@ public class LijiangMapActivity extends BaiduMapActivity implements Observer{
 		 Intent i = new Intent(this,AddAlarmActivity.class);
 		 i.putExtra(AddAlarmActivity.ALARM_TITLE, title);
 		 i.putExtra(AddAlarmActivity.ALARM_MESSAGE, message);
-		 i.putExtra(AddAlarmActivity.ALARM_LONGITUDEE6, latestPoint.getLongitudeE6());
-		 i.putExtra(AddAlarmActivity.ALARM_LATITUDEE6, latestPoint.getLatitudeE6());
+		 i.putExtra(AddAlarmActivity.ALARM_LONGITUDEE6, tappedPoint.getLongitudeE6());
+		 i.putExtra(AddAlarmActivity.ALARM_LATITUDEE6, tappedPoint.getLatitudeE6());
 		 
 		 this.startActivity(i);
 	}
-	
+
 	private String getPostionName(){
 		return poiNameEditText.getText().toString().trim();
 	}
@@ -171,25 +97,6 @@ public class LijiangMapActivity extends BaiduMapActivity implements Observer{
 		progressDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
 		progressDialog.setMessage(getString(R.string.searching));
 		progressDialog.setCancelable(false);
-	}
-	
-	private void initialMapView(){
-		mMapView = (MapView)findViewById(R.id.bmapView);
-	    mMapView.setBuiltInZoomControls(true);
-	    mMapView.setDrawOverlayWhenZooming(true);
-	    
-	    customOverlay = 
-	    		new BaiduLongPressItemizedOverlay(this,this.getResources().getDrawable(R.drawable.marker_rounded_grey));
-		searchOverlay = 
-				new BaiduItemizedOverlay(LijiangMapActivity.this,getResources().getDrawable(R.drawable.marker_rounded_red));		  
-		userOverlay = 
-				new BaiduItemizedOverlay(LijiangMapActivity.this,getResources().getDrawable(R.drawable.marker_rounded_blue));
-		
-		mMapView.getOverlays().add(customOverlay);
-		mMapView.getOverlays().add(searchOverlay);
-		mMapView.getOverlays().add(userOverlay);
-	    
-	    attachPopView();
 	}
 	
 	private void initialSearch(){
@@ -242,16 +149,5 @@ public class LijiangMapActivity extends BaiduMapActivity implements Observer{
 				}
 		    });
 	}
-	
-	private void attachPopView(){
-		mPopView=super.getLayoutInflater().inflate(R.layout.popview2, null);
-		popName=(TextView)mPopView.findViewById(R.id.popName);
-		popAddress=(TextView)mPopView.findViewById(R.id.popAddress);
-		
-		mMapView.addView( mPopView,
-                new MapView.LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT,
-                		null, MapView.LayoutParams.BOTTOM_CENTER));
-		mPopView.setVisibility(View.GONE);
-	}
-	
+
 }
